2bde646ce159790fc114a39fd6fd1f11b53eb750
Carolina <carolinazapatavidal@gmail.com>
2022-04-21 16:18:03 -0600

    char

--- src/data.js
+++ src/data.js
@@ -1,31 +1,58 @@
 import data from './data/harrypotter/harry.js';
 
 let characters = data.characters;
-let strName = characters.dataset.death;
-console.log(strName)
+// let name = Array.from(characters);
+console.log(characters[1])
 
 
-//for (const property in object) {
- //   console.log(`${property}: ${object[property]}`);
+// for (const property in object) {
+//    console.log(`${property}: ${object[property]}`);
 //  }
 
-//console.log(characters)
+// console.log(characters)
 
-const root = document.getElementById('root');
+// const root = document.getElementById('root');
 
-  const lista = document.createElement('ul');
+//   const lista = document.createElement('ul');
   
-  lista.textContent = `${strName}`;
-  root.appendChild (lista);
+//   lista.textContent = `${strName}`;
+//   root.appendChild (lista);
 
 
-// function fetchHarry(id) {
-//   fetch("./data/harrypotter/harry.json")
+// function fetchHarry(characters) {
+//   fetch(`data/harrypotter/harry.json/${characters}/`)
 //     .then((res) => res.json())
-//     .then((data) => {
-//       createHarry(data);
-//     })
+//     .then((data) => console.log(data));
+//     }
+
+// function fetchName(offset, limit) {
+//   spinner.style.display = "block";
+//   for (let i = offset; i <= offset + limit; i++) {
+//     fetchHarry(i);
 //   }
+// }
+    
+// function createCard(characters) {
+//   const flipCard = document.createElement("div");
+//   flipCard.classList.add("flip-card");
+    
+//   const cardContainer = document.createElement("div");
+//   cardContainer.classList.add("card-container");
+    
+//   flipCard.appendChild(cardContainer);
+// }
+// function fetchNames(name) {
+//   fetch(`./data/harrypotter/harry.json/characters/${name}/`)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+//     }
+
+//     fetchNames();
+// function fetchNames(name) {
+//   for (let i = 1; i <= Number; i++) {
+//     fetchHarry(i);
+// }
+// }
 
 
 
@@ -34,3 +61,4 @@ const root = document.getElementById('root');
 // export const anotherExample = () => {
 //   return 'OMG';
 // };
+
