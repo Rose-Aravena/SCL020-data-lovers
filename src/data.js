3e42f5e639f592697bcbf7d6a1b6a375a03e6d65
Carolina <carolinazapatavidal@gmail.com>
2022-04-25 09:21:06 -0600

    DIESISEIS

--- src/data.js
+++ src/data.js
@@ -2,40 +2,6 @@ import data from './data/harrypotter/harry.js';
 
 let characters = data.characters;
 
-
-const result = characters.filter(character => character.house === 'Gryffindor')
-
-console.log('Result', result);
-
-characters.forEach(function(character){
-const cardContainer = document.getElementById('cardContainer');
-const list = document.createElement('h2');
-const house = document.createElement('h3');
-const birth = document.createElement('h4');
-const wand = document.createElement('h5');
-const patronus =document.createElement('h6');
-
-list.classList.add("list");
-  list.textContent = character.name;
-  cardContainer.appendChild (list);
-  
-  house.textContent = character.house;
-  cardContainer.appendChild (house);
-
-  birth.textContent = character.birth;
-  cardContainer.appendChild (birth);
-
-  wand.textContent = character.wand;
-  cardContainer.appendChild (wand);
-
-  patronus.textContent = character.patronus;
-  cardContainer.appendChild (patronus);
-  
-});
-
-// const sorts = characters.sort((a,b)=>a-b);
-
-// console.log('Sorts', sorts);
 let spells = data.spells;
 
 let potions = data.potions;
@@ -43,6 +9,57 @@ let potions = data.potions;
 let books = data.books;
 
 let funFacts = data.funFacts;
+
+const result = characters.filter(character => character.house === 'Gryffindor')
+
+console.log('Result', result);
+
+characters.forEach(function(character){
+const cardContainer = document.getElementById('cardContainer');
+
+const card = document.createElement('div');
+card.classList.add('cardDiv');
+
+const name = document.createElement('h2');
+name.classList.add('nameDiv');
+
+const house = document.createElement('p');
+house.classList.add('houseDiv');
+
+const birth = document.createElement('p');
+birth.classList.add('birthDiv');
+
+const wand = document.createElement('p');
+wand.classList.add('wandDiv');
+
+const patronus =document.createElement('p');
+patronus.classList.add('patronusDiv');
+
+  name.textContent = character.name;
+  card.appendChild (name);
+  
+  house.textContent = character.house;
+  card.appendChild (house);
+
+  birth.textContent = character.birth;
+  card.appendChild (birth);
+
+  wand.textContent = character.wand;
+  card.appendChild (wand);
+
+  patronus.textContent = character.patronus;
+  card.appendChild (patronus);
+
+  cardContainer.appendChild (card);
+  
+});
+
+
+
+// const sorts = characters.sort((a,b)=>a-b);
+
+// console.log('Sorts', sorts);
+
 
 
 // const cursor = document.querySelector('cursor');
