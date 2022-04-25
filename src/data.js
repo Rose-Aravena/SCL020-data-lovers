221b75a7767fe695dde9f910c249a124e13aa99a
Carolina <carolinazapatavidal@gmail.com>
2022-04-25 11:27:44 -0600

    mainjs

--- src/data.js
+++ src/data.js
@@ -1,61 +1,9 @@
-import data from './data/harrypotter/harry.js';
 
-let characters = data.characters;
-
-let spells = data.spells;
-
-let potions = data.potions;
-
-let books = data.books;
-
-let funFacts = data.funFacts;
-
-const result = characters.filter(character => character.house === 'Gryffindor')
-
-console.log('Result', result);
-
-characters.forEach(function(character){
-const cardContainer = document.getElementById('cardContainer');
-
-const card = document.createElement('div');
-card.classList.add('cardDiv');
-
-const name = document.createElement('h2');
-name.classList.add('nameDiv');
-
-const house = document.createElement('p');
-house.classList.add('houseDiv');
-
-const birth = document.createElement('p');
-birth.classList.add('birthDiv');
-
-const wand = document.createElement('p');
-wand.classList.add('wandDiv');
-
-const patronus =document.createElement('p');
-patronus.classList.add('patronusDiv');
-
-  name.textContent = character.name;
-  card.appendChild (name);
-  
-  house.textContent = character.house;
-  card.appendChild (house);
-
-  birth.textContent = character.birth;
-  card.appendChild (birth);
-
-  wand.textContent = character.wand;
-  card.appendChild (wand);
-
-  patronus.textContent = character.patronus;
-  card.appendChild (patronus);
-
-  cardContainer.appendChild (card);
-  
-});
+export default function gryffindor(house){
+  characters.filter(character => character.house === 'Gryffindor')};
 
 
-
+// export default function (character)
 // const sorts = characters.sort((a,b)=>a-b);
 
 // console.log('Sorts', sorts);
