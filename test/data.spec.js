38967def8aade135d7eb9a816fc7c584cf0b22aa
Carolina <carolinazapatavidal@gmail.com>
2022-05-10 11:33:16 -0600

    holi

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,23 +1,26 @@
-import { example, anotherExample } from '../src/data.js';
+import { filterHouse, filterSpells, createCharacter, createSpell, createPotion, createBook } from '../src/data.js';
+import data from './data/harrypotter/harry.js';
 
-
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
-  });
-
-  it('returns `example`', () => {
-    expect(example()).toBe('example');
-  });
-});
-
-
-describe('anotherExample', () => {
-  it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
-  });
-
-  it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+describe('filterHouse-Debe filtrar todos los characters por casa', () => {
+  
+  test('funcion filtrado', () => {
+    const characters = data.characters
+    const filtered = filterHouse(characters, 'Slytherin')
+    expect(filtered ).toContain(characters);
   });
 });
+//   it('returns `characters.filter', () => {
+//     expect(characters.filter()).toBe('function');
+//   });
+// });
+
+
+// describe('anotherExample', () => {
+//   it('is a function', () => {
+//     expect(typeof anotherExample).toBe('function');
+//   });
+
+//   it('returns `anotherExample`', () => {
+//     expect(anotherExample()).toBe('OMG');
+//   });
+// });
